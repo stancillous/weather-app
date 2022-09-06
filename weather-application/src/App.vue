bf4d510d0cf26b77c028a10684bbb7ded051de08
raymond <stancillousray@gmail.com>
2022-09-06 22:59:01 +0300

    updatedFiles

---
+++ weather-application/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div>
+    <WeatherDetailsComp/>
+
+
+  </div>
+
+
+
+
+</template>
+
+<script>
+import WeatherDetailsComp from './components/WeatherDetails.vue'
+  
+
+export default {
+  name: 'App',
+  components: {
+    WeatherDetailsComp,
+
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ weather-application/src/components/CityName.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <div>
+        <input v-model="cityName" @keyup.enter="$emit('passCityName',cityName)" autofocus type="text" placeholder="Enter city name">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CityNameComp',
+        emits:['passCityName'],
+        data(){
+            return{
+                cityName:''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    div{
+        text-align: center;
+    }
+    input{
+            border: 1px solid black;
+            padding: 1rem 2rem;
+            font-weight: 800;
+            font-family: "Satoshi", sans-serif, "General Sans", sans-serif;
+            font-size: 1.5rem;
+            background-color: rgb(211, 207, 207);
+            color:rgb(0, 0, 0);
+            width: 100%;
+        }
+
+</style>
